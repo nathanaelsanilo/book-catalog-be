040350d69f16e16081a3cf10f9ef7928a22620e9
nathanaelsanilo <nathanaelsanilo@gmail.com>
2024-01-18 20:49:29 +0700

    build(env): add db config env variables

--- src/database/providers/database.providers.ts
+++ src/database/providers/database.providers.ts
@@ -1,17 +1,20 @@
 import { DataSource } from 'typeorm';
 import { DatabaseProviderKey } from '../constants';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 export const databaseProviders = [
   {
+    imports: [ConfigModule],
+    inject: [ConfigService],
     provide: DatabaseProviderKey.DataSource,
-    useFactory: async () => {
+    useFactory: async (configService: ConfigService) => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'localhost',
+        host: configService.get('DB_HOST'),
         port: 8889,
-        username: 'root',
-        password: 'root',
-        database: 'book_catalog',
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
         entities: [__dirname + '/../../**/entity/*.entity{.ts,.js}'],
 
         // WARNING: do not use in production
